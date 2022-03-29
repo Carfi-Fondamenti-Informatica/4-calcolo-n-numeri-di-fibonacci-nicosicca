bbbacc3839f330fc58af29d779ebd23e5efd64cb
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-03-29 12:14:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   int n,t=1,ts=0,td=1;
+   cin>>n;
+   
+   if(n>=2)
+   {
+      for(int i=0;i<n;i++)
+      {
+         cout<<t<<endl;
+         
+         t=ts+td;
+         ts=td;
+         td=t;
+      }
+   }
+   else
+      cout<<"errore"<<endl;
+   
    return 0;
 }
